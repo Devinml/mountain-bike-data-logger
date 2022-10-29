027530371d803e3ac3a0a2aba88c2f4b9ce85899
Devin Link <devin.link@genesys.com>
2022-10-29 10:27:38 -0700

    added button pushing to start logging, init for the file name has been moved to the loop so it writes a new file

--- data_logger/data_logger.ino
+++ data_logger/data_logger.ino
@@ -15,20 +15,23 @@ Adafruit_LIS3DH lis_2 = Adafruit_LIS3DH();
 
 // how many milliseconds between grabbing data and logging it. 1000 ms is once a second
 #define LOG_INTERVAL  5 // mills between entries (reduce to take more/faster data)
-
-// how many milliseconds before writing the logged data permanently to disk
-// set it to the LOG_INTERVAL to write each time (safest)
-// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to 
-// the last 10 reads if power is lost but it uses less power and is much faster!
 #define SYNC_INTERVAL 50 // mills between calls to flush() - to write data to the card
 uint32_t syncTime = 0; // time of last sync()
 
 #define ECHO_TO_SERIAL   1 // echo data to serial port
 #define WAIT_TO_START    0 // Wait for serial input in setup()
 
-// the digital pins that connect to the LEDs
 #define redLEDpin 13
 #define greenLEDpin 8
+const int buttonPin = 2;  
+int ledState = LOW;        // the current state of the output pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin
+bool write_init = true;
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 
 // The analog pins that connect to the sensors
 
@@ -55,10 +58,11 @@ void setup(void)
 {
   Serial.begin(9600);
   Serial.println();
-  
+  pinMode(buttonPin, INPUT);
   // use debugging LEDs
   pinMode(redLEDpin, OUTPUT);
   pinMode(greenLEDpin, OUTPUT);
+  digitalWrite(redLEDpin, ledState);
   if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
     Serial.println("Couldnt start");
     error("Could not find First accelerometer");
@@ -88,74 +92,110 @@ void setup(void)
     logfile.println("RTC failed");
   }
   rtc.start();
-  DateTime now = rtc.now();
-  String date;
-  date =  String(now.month()) + "-" + String(now.day()) + "-"+  String(now.minute())+ ".csv";
-  char filename[16] = {0};    
-  date.toCharArray(filename, 16);
-
-  Serial.println("filename: " + String(filename));
-    
-  if (! SD.exists(filename)) {
-      // only open a new file if it doesn't exist
-    logfile = SD.open(filename, FILE_WRITE); 
-   }
   
-  
-  if (! logfile) {
-    error("couldnt create file");
-  }
-  
-  Serial.print("Logging to: ");
-  Serial.println(filename);
-
-  logfile.println("millis,x_1,y_1,z_1,x_2,y_2,z_2"); 
   analogReference(EXTERNAL);
 }
 
 void loop(void)
 {
-  lis.read();      // get X Y and Z data at once
-  lis_2.read();
-  sensors_event_t event;
-  lis.getEvent(&event);
-  sensors_event_t event_2;
-  lis_2.getEvent(&event_2);
+  int reading = digitalRead(buttonPin);
 
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
 
-  // delay for the amount of time we want between readings
-  delay((LOG_INTERVAL - 1) - (millis() % LOG_INTERVAL));
-  
-  digitalWrite(greenLEDpin, HIGH);
-  
-  // log milliseconds since starting
-  uint32_t m = millis();
-  logfile.print(m);           // milliseconds since start
-  logfile.print(",");
-  logfile.print(event.acceleration.x);
-  logfile.print(",");
-  logfile.print(event.acceleration.y);
-  logfile.print(",");
-  logfile.print(event.acceleration.z);
-  logfile.print(",");
-  logfile.print(event_2.acceleration.x);
-  logfile.print(",");
-  logfile.print(event_2.acceleration.y);
-  logfile.print(",");
-  logfile.print(event_2.acceleration.z);
-  logfile.println();
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
 
-  digitalWrite(greenLEDpin, LOW);
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
 
-  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
-  // which uses a bunch of power and takes time
-  if ((millis() - syncTime) < SYNC_INTERVAL) return;
-  syncTime = millis();
-  
-  // blink LED to show we are syncing data to the card & updating FAT!
-  digitalWrite(redLEDpin, HIGH);
-  logfile.flush();
-  digitalWrite(redLEDpin, LOW);
-  
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+        write_init = true;        
+      }
+    }
+  }
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+  Serial.println("Write_init value: ");
+  Serial.print(write_init);
+  if (ledState){
+    if (write_init){
+      Serial.println("in the init of data logging");
+      DateTime now = rtc.now();
+      String date;
+      date =  String(now.month()) + "-" + String(now.day()) + "-"+  String(now.minute())+ ".csv";
+      char filename[16] = {0};    
+      date.toCharArray(filename, 16);
+
+      Serial.println("filename: " + String(filename));
+        
+      if (! SD.exists(filename)) {
+          // only open a new file if it doesn't exist
+        Serial.println("file does not exist creating a new one");
+        logfile = SD.open(filename, FILE_WRITE); 
+      }
+      if (! logfile) {
+        error("couldnt create file");
+      }
+      
+      Serial.print("Logging to: ");
+      Serial.println(filename);
+
+      logfile.println("millis,x_1,y_1,z_1,x_2,y_2,z_2");
+      write_init = false; 
+    }
+    Serial.println("logging data");
+    lis.read();      // get X Y and Z data at once
+    lis_2.read();
+    sensors_event_t event;
+    lis.getEvent(&event);
+    sensors_event_t event_2;
+    lis_2.getEvent(&event_2);
+
+    // delay for the amount of time we want between readings
+    delay((LOG_INTERVAL - 1) - (millis() % LOG_INTERVAL));
+    
+    digitalWrite(greenLEDpin, HIGH);
+    
+    // log milliseconds since starting
+    uint32_t m = millis();
+    logfile.print(m);           // milliseconds since start
+    logfile.print(",");
+    logfile.print(event.acceleration.x);
+    logfile.print(",");
+    logfile.print(event.acceleration.y);
+    logfile.print(",");
+    logfile.print(event.acceleration.z);
+    logfile.print(",");
+    logfile.print(event_2.acceleration.x);
+    logfile.print(",");
+    logfile.print(event_2.acceleration.y);
+    logfile.print(",");
+    logfile.print(event_2.acceleration.z);
+    logfile.println();
+
+    digitalWrite(greenLEDpin, LOW);
+
+    // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
+    // which uses a bunch of power and takes time
+    if ((millis() - syncTime) < SYNC_INTERVAL) return;
+    syncTime = millis();
+    
+    // blink LED to show we are syncing data to the card & updating FAT!
+    digitalWrite(redLEDpin, HIGH);
+    logfile.flush();
+    digitalWrite(redLEDpin, LOW);
+  }
 }
-
