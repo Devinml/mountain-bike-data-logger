939e904356ce2f66352ba77354a7fec70afccb37
Devin Link <devin.link@genesys.com>
2022-10-30 18:09:03 -0700

    updated plots

--- plot_scripts/plot_acc.py
+++ plot_scripts/plot_acc.py
@@ -16,21 +16,32 @@ def calculate_time_ran(df):
     last = df['millis'].iloc[-1]
     print(f'{(last-first) / 1000} seconds')
 
-def calculate_logging_statistics(df):
+def compute_differences(df):
     df['difference'] = df['millis'].diff()
+    return df
+
+def calculate_logging_statistics(df):
+    df = compute_differences(df)
     print('log statisics')
     print(f'mean: {df["difference"].mean()}')
     print(f'variance: {df["difference"].var()}')
     print(f'max: {df["difference"].max()}')
     print(f'min: {df["difference"].min()}')
+    print(f'total time: {calculate_time_ran(df)}')
     try:
         print(f"log_interval: {df['LOG_INTERVAL'].iloc[0]}")
         print(f'sync interval {df["SYNC_INTERVAL"].iloc[0]}')
         print(f'sync_time {df["syncTime"].iloc[0]}')
     except Exception as e:
         print("intervals not found")
-
     print('\n')
+
+def plot_indexs_for_dir():
+    for file_ in os.listdir('data'):
+        df = read_file(file_)
+        df = compute_differences(df)
+        
+        # plot_index_interval(df, file_, 'difference')
 
 def compute_stats_of_all_files():
     for file_ in os.listdir('data'):
@@ -38,14 +49,15 @@ def compute_stats_of_all_files():
         df = read_file(file_)
         calculate_logging_statistics(df)
 
-def plot_index_interval(df, file_name):
+def plot_index_interval(df, file_name, column='millis'):
     fig, ax = plt.subplots()
-    df['millis'].plot(ax=ax)
+    df[column].plot(ax=ax)
     split_name = file_name.split('.')[0]
     plt.savefig(f'imgs/{split_name}.jpeg')
 
 if __name__ == '__main__':
     compute_stats_of_all_files()
+    plot_indexs_for_dir()
     # file_name = '10-30-56.csv'
     # plot = False
     # df = read_file(file_name)
