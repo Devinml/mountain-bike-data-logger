fd4d7f8a74759b258df915fb2f6f9e30a1133c79
Devin Link <devin.link@genesys.com>
2022-11-12 09:39:30 -0800

    adding fileparser

--- plot_scripts/parse_bin.py
+++ plot_scripts/parse_bin.py
@@ -1,4 +1,27 @@
 import struct 
-data = open("162947.txt", "rb").read()
-buffer = data[0:28]
-struct.unpack("iffffff", buffer)
+
+class FileDeconstructor:
+    def __init__(self, fp, buffer_size=28, struct_format="iffffff") -> None:
+        self.fp = fp
+        self.buffer_size=buffer_size
+        self.struct_format = struct_format
+        
+    def process_file(self, fp=None):
+        if fp is None:
+            fp = self.fp
+        data = open(fp, "rb").read()
+        start = 0
+        end = self.buffer_size
+        for _ in range(3):
+            buffer = data[start: end]
+            print(struct.unpack(self.struct_format, buffer))
+            start += self.buffer_size
+            end += self.buffer_size
+
+
+
+        
+if __name__ == '__main__':
+    fp =   "data/163145.txt"
+
+    FileDeconstructor(fp).process_file()
